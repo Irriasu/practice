35ddfda900465059dfd097224dafd0ddd06311a9
Iriasu <ahssan.ilyas@ensi.ma>
2024-06-12 22:30:54 +0100

    tt

--- microservice.js
+++ microservice.js
@@ -1,1 +1,10 @@
-console.log("I'm CI/CD project")
+function sum(a, b) {
+    return a + b;
+  }
+  
+  const num1 = 5;
+  const num2 = 7;
+  
+  const result = sum(num1, num2);
+  
+  console.log(`The sum of ${num1} and ${num2} is ${result}`);
